2d302ced27c7045be53b4ce35cacd19a7c71b2f8
rundmcook <rundmcook@gmail.com>
2025-04-22 17:39:57 -0400

    Update GPUController.java

--- GPUController.java
+++ GPUController.java
@@ -5,36 +5,60 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+/**
+*@author: Group 2
+*CSC331
+*04/22/25
+*Purpose: Creates an interface for the user to enter specifications for a new GPU object
+*/
 public class GPUController {
 
+    //Object used to hold the current components of the PC
     private PCBuild currentBuild;
 
+    /**
+    *Sets a new build for the PCBuild object
+    *@param build - new build set for the PCBuild
+    */
     public void setBuild(PCBuild build){
         this.currentBuild = build;
     }
+
+    //Text Field for brand
     @FXML
     private TextField brandField;
 
+    //Text Field for clock speed
     @FXML
     private TextField clockSpeedField;
 
+    //Text field for model
     @FXML
     private TextField modelField;
 
+    //Text Field for output
     @FXML
     private TextArea outputArea;
 
+    //Text Field for power consumption
     @FXML
     private TextField powerConsumptionField;
 
+    //Text Field for price
     @FXML
     private TextField priceField;
 
+    //Text Field for vRAM
     @FXML
     private TextField vRamField;
 
+    /**
+    *Method used to handle the creation of a new GPU object using details entered in a GUI by the user
+    *@param event - triggered when the user has entered all info and pressed the create button
+    */
     @FXML
     void handleAddGPU(ActionEvent event) {
+        //Creates a new GPU, prompts users to try again if an invalid input is entered
         try{
             String brand = brandField.getText();
             String model = modelField.getText();
