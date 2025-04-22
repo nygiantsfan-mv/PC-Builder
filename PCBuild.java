4c27a42bfa53254d469004b118e2e706205f9e5b
nygiantsfan-mv <matthewjvoisey@gmail.com>
2025-04-22 17:39:00 -0400

    Update PCBuild.java

--- PCBuild.java
+++ PCBuild.java
@@ -1,6 +1,8 @@
 package com.example.pcbuilderfx;
 
 public class PCBuild {
+    // attributes will be used to manage user's current pc build
+    // each attribute will manage a specific component of the build
     private CPU cpu;
     private GPU gpu;
     private RAM ram;
@@ -9,46 +11,97 @@ public class PCBuild {
     private Motherboard motherboard;
 
     //getters
+    /**
+    * method retrieves the object's CPU component
+    * @return cpu: the cpu component of the user's pc build
+    */
     public CPU getCpu(){
         return cpu;
     }
+    /**
+    * method retrieves the object's GPU component
+    * @return gpu: the gpu component of the user's pc build
+    */
     public GPU getGpu(){
         return gpu;
     }
+    /**
+    * method retrieves the object's RAM component
+    * @return ram: the ram component of the user's pc build
+    */
     public RAM getRam(){
         return ram;
     }
+    /**
+    * method retrieves the object's Storage component
+    * @return: storage: the storage component of the user's pc build
+    */
     public Storage getStorage(){
         return storage;
     }
+    /**
+    * method retrieves the object's PowerSupply component
+    * @return: powerSupply: the powersupply component of the user's pc build
+    */
     public PowerSupply getPowerSupply(){
         return powerSupply;
     }
+    /**
+    * method retrieves the object's Motherboard component
+    * @return: motherboard: the motherboard component of the user's pc build
+    */
     public Motherboard getMotherboard(){
         return motherboard;
     }
 
     // setters
+    /**
+    * method sets the objects CPU component
+    * @param cpu: string representation of the cpu component
+    */
     public void setCPU(CPU cpu){
         this.cpu = cpu;
     }
+    /**
+    *method sets the objects GPU component
+    * @param gpu: String representation of the gpu component
+    */
     public void setGpu(GPU gpu){
         this.gpu = gpu;
     }
+    /**
+    *method sets the objects RAM component
+    * @param ram: String representation of the ram component
+    */
     public void setRam(RAM ram){
         this.ram = ram;
     }
+    /**
+    * method sets the objects storage component
+    * @param storage: string representation of the storage component
+    */
     public void setStorage(Storage storage){
         this.storage = storage;
     }
+    /**
+    *method sets the objects PowerSupply component
+    * @param powerSupply: string representing a powersupply component
+    */
     public void setPowerSupply(PowerSupply powerSupply){
         this.powerSupply = powerSupply;
     }
+    /**
+    * method sets the objects Motherboard component
+    * @ param motherboard: string representing a motherboard component
+    */
     public void setMotherboard(Motherboard motherboard){
         this.motherboard = motherboard;
     }
 
-    //imported from PCBUILDERFX
+    /**
+    * method calculates the total price of all the PCBuild object's components
+    * @return double: total price of the pc components
+    */
     public double calculateTotalPrice() {
         double total = 0.0;
         if (cpu != null) total += cpu.getPrice();
