0b3a3b1a7f0d01e61b5a96715996e9147c750194
Jake Hamilton <jh0695@gmail.com>
2025-04-22 17:33:46 -0400

    Update CPUController.java

--- CPUController.java
+++ CPUController.java
@@ -5,31 +5,55 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+/**
+* Controller class for handling user interaction with the CPU in the PC Builder application.
+* This class allows the user to input CPU specifications and add a CPU to the current build.
+*/
+
 public class CPUController {
 
+    /** Current PC  build to which the CPU will be added */
     private PCBuild currentBuild;
+
+    /**
+    * Sets the current PC instance
+    * 
+    * @param build the PCBuild instance to associate with this controller
+    */
 
     public void setBuild(PCBuild build){
         this.currentBuild = build;
     }
+    
+    /** Textfield for entering the CPU brand */
     @FXML
     private TextField brandField;
 
+    /** Textfield for entering the CPU model */
     @FXML
     private TextField modelField;
 
+    /** Textfield for entering the CPU outputArea */
     @FXML
     private TextArea outputArea;
 
+    /** Textfield for entering the CPU priceField */
     @FXML
     private TextField priceField;
 
+    /** Textfield for entering the CPU socketTypeField */
     @FXML
     private TextField socketTypeField;
 
+    /** Textfield for entering the CPU speedField */
     @FXML
     private TextField speedField;
 
+    /** Handles the action of adding a CPU to the current build when user submits
+    * Validates input fields, creates new CPU object, updates the build, displays
+    *
+    * @param event the ActionEvent triggered by the user
+    */
     @FXML
     void handleAddCpu(ActionEvent event) {
         try{
