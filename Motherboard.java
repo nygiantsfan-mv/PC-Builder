57f831e9c033c5333c84890252bf2084e3bc1ff2
Noah <128927291+NMPreston@users.noreply.github.com>
2025-04-22 17:53:11 -0400

    Update Motherboard.java

--- Motherboard.java
+++ Motherboard.java
@@ -1,7 +1,7 @@
 /**
  * @author: Group2
  * Date: 4/22/2025
- * Class: CSC331
+ * Class: CSC-331
  * Purpose: Motherboard is a subclass to Component. It represents the motherboard in a computer.
  */
 package com.example.pcbuilderfx;
