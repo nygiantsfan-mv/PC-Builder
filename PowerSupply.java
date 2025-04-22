a2ceefa321fd56ca35aa83305ed217063074590c
Noah <128927291+NMPreston@users.noreply.github.com>
2025-04-22 17:26:50 -0400

    Update PowerSupply.java

--- PowerSupply.java
+++ PowerSupply.java
@@ -1,5 +1,5 @@
 /**
- * @author: Isaiah Goddard
+ * @author: Group2
  * Date: 4/16/2025
  * Class: CSC-331-001
  * Purpose: Power Supply is a subclass to Component. It represents the power supply in a computer.
