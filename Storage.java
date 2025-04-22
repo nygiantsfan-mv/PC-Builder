fbb59e10884a13ecba6683012d7626ea69c62b95
Noah <128927291+NMPreston@users.noreply.github.com>
2025-04-22 17:54:16 -0400

    Update Storage.java

--- Storage.java
+++ Storage.java
@@ -1,7 +1,7 @@
 /**
  * @author: Group2
  * Date: 4/22/2025
- * Class: CSC331
+ * Class: CSC-331
  * Purpose: Represents storage component. Inherits from the component class
  */
 package com.example.pcbuilderfx;
